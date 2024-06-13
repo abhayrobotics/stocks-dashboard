d88d8e409943b9d1ce9225d724ce87fd805aaffe
Abhay kumar <abhayrobotics1908@gmail.com>
2024-06-14 00:49:23 +0530

    wuery not working

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,21 +1,34 @@
-import React, { useEffect } from "react";
-
+import { useEffect, useRef } from "react";
+import { apikey } from "../utils/constant";
 
 const DashBoard = () => {
-  useEffect(() => {
-    handleSearch();
-  }, []);
-
-  const handleSearch = async () => {
-    const data = await fetch(
-      "https://financialmodelingprep.com/api/v3/income-statement/tcs?period=annual&apikey=gZW5k797olakipqjIPGqToAsnotncqpv"
-    );
-    const json = await data.json();
-    console.log(json);
   
+  const query = useRef("5");
+  const handleSearch = () => {
+    console.log(query)
+    
+    
+    const fetchQuery = async () => {
+      const URL =
+      "https://financialmodelingprep.com/api/v3/search-ticker?query=" +
+      query +
+      "&limit=10&exchange=BSE&apikey=gZW5k797olakipqjIPGqToAsnotncqpv";
+      
+      const data = await fetch(URL);
+      const json = await data.json();
+      console.log(json);
+      };
+    fetchQuery();
   };
-
-  return <div>DashBoard</div>;
+  console.log(process.env.REACT_APP_STOCKS_API);
+  return (
+    <div>
+      <form onSubmit={(e)=>e.preventDefault()}>
+        <input className="border" type="text" ref={query} />
+        <button onClick={handleSearch}>Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default DashBoard;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,3 @@
-export const Search_URL ='https://yahoo-finance127.p.rapidapi.com/search/aa'
 
-export const yahooKey ="167450fecamsh5cb0275c68f1681p1be1c5jsn8d46840f566c";
 
-export const apikey="gZW5k797olakipqjIPGqToAsnotncqpv"
+export const apikey= process.env.REACT_APP_STOCKS_API
